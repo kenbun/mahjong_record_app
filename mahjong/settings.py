d55260522775192d8b5c03de5c6cdd284cef5ff8
kenbun <takefumic@gmail.com>
2022-04-06 22:47:08 +0900

    fit heroku

--- mahjong/settings.py
+++ mahjong/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-83*-cbv*+p%w)xmbpx4&q%3t9&jf1e@g7d%ijs+i7=&k!d828j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
